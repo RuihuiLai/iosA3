69a15f71f7ede54c41ade99bb0d8019c3c7021a7
RuihuiLai <Ruihui.Lai-1@student.uts.edu.au>
2022-05-08 17:34:21 +0800

    version 1.0

---
+++ BookingSystem/BookingSystem/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  BookingSystem
+//
+//  Created by mos on 2022/5/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    sadfasdf
+    a
+    sfa
+    df
+    a
+
+}
+
